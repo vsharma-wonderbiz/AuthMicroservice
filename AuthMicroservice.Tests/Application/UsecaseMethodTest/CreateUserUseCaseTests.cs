7a122669638b1b09ba13ec78620c4cd48ab192b6
Vinay Sharma <vinay.sharma@wonderbiz.in>
2026-01-06 17:53:25 +0530

    learning and adding unit test cases for the project

---
+++ AuthMicroservice.Tests/Application/UsecaseMethodTest/CreateUserUseCaseTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuthMicroservice.Domain.Interface;
+using AutoMapper;
+using Moq;
+
+namespace AuthMicroservice.Tests.Application.UsecaseMethodTest
+{
+    public class CreateUserUseCaseTests
+    {
+        private readonly Mock<IUserRepository> _mockUserRepository;
+        private readonly Mock<IMapper> _mockMapper;
+       
+
+        public CreateUserUseCaseTests()
+        {
+            _mockUserRepository = new Mock<IUserRepository>();
+            _mockMapper = new Mock<IMapper>();
+           
+        }
+    }
+}
